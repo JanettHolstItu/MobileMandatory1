39a2a934d6c3293a538db05c3d468998b0331655
sbrerup <sbrerup@gmail.com>
2014-09-13 16:58:40 +0200

    RFC862Server

---
+++ Handin files/RFC862Server.java
@@ -0,0 +1,37 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class RFC862Server {
+
+    public static void main(String[] args) {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket(7007);
+            System.out.println("Listening at " + aSocket.getLocalAddress()
+                    + " : " + aSocket.getLocalPort());
+            byte[] buffer = new byte[1000];
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                System.out.println("Listening...");
+                aSocket.receive(request);
+                System.out.println("Received " + request.getLength()
+                        + " bytes from" + request.getAddress().toString()
+                        + " : " + request.getPort());
+                DatagramPacket reply = new DatagramPacket(request.getData(),
+                        request.getLength(), request.getAddress(), request.getPort());
+                aSocket.send(reply);
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (aSocket != null) {
+                aSocket.close();
+            }
+        }
+    }
+}
